eaf2ba9f1f739a0aed3d50ccff41a46e1172698e
Ian C <31007296+Minibeast@users.noreply.github.com>
2020-10-06 17:14:49 -0400

    init

---
+++ Component.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using LiveSplit.Model;
+using LiveSplit.Options;
+using Discord;
+
+namespace LiveSplit.UI.Components
+{
+    public class DiscordIntegration : LogicComponent
+    {
+        public override string ComponentName => "Discord Rich Presence";
+
+        public Discord.Discord discord;
+
+        public TimerPhase LastRunningState;
+        public string LastCategory;
+        public string LastGame;
+
+        public DiscordIntegration(LiveSplitState state)
+        {
+            discord = new Discord.Discord(763054362107838504, (UInt64)Discord.CreateFlags.Default);
+
+            UpdatePresence(state.CurrentPhase, state.Run.CategoryName, state.Run.GameName);
+
+        }
+
+        public override void Dispose()
+        {
+            discord.Dispose();
+        }
+
+        public void UpdatePresence(TimerPhase RunState, string CategoryName, string GameName)
+        {
+            LastRunningState = RunState;
+            LastCategory = CategoryName;
+            LastGame = GameName;
+
+            var activityManager = discord.GetActivityManager();
+
+            string RunningState;
+
+            if (RunState == TimerPhase.NotRunning)
+                RunningState = "Not Running";
+            else if (RunState == TimerPhase.Running)
+                RunningState = "Running";
+            else if (RunState == TimerPhase.Paused)
+                RunningState = "Paused";
+            else
+                RunningState = "Ended";
+
+
+            var activity = new Discord.Activity
+            {
+                Details = GameName,
+                State = CategoryName,
+                Assets =
+                {
+                    LargeImage = "livesplit_icon",
+                    LargeText = RunningState
+                }
+            };
+            activityManager.UpdateActivity(activity, (res) =>
+            {
+                if (res != Discord.Result.Ok)
+                    throw new ResultException(res);
+            });
+        }
+
+        public override Control GetSettingsControl(LayoutMode mode)
+        { return null; }
+
+        public override XmlNode GetSettings(XmlDocument document)
+        { return null; }
+
+        public override void SetSettings(XmlNode settings) { }
+
+        public override void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+        {
+            if (state.CurrentPhase != LastRunningState || state.Run.CategoryName != LastCategory || state.Run.GameName != LastGame)
+            {
+                UpdatePresence(state.CurrentPhase, state.Run.CategoryName, state.Run.GameName);
+            }
+
+            discord.RunCallbacks();
+        }
+    }
+}
---
+++ Factory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LiveSplit.Model;
+using LiveSplit.UI.Components;
+
+[assembly: ComponentFactory(typeof(Factory))]
+
+namespace LiveSplit.UI.Components
+{
+    public class Factory : IComponentFactory
+    {
+        public string ComponentName => "Discord Rich Presence";
+        public string Description => "Discord Rich Presence Integration";
+        public ComponentCategory Category => ComponentCategory.Other;
+        public Version Version => Version.Parse("1.8.0");
+
+        public string UpdateName => ComponentName;
+        public string UpdateURL => "http://livesplit.org/update/";
+        public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.ScriptableAutoSplit.xml";
+
+        public IComponent Create(LiveSplitState state) => new DiscordIntegration(state);
+    }
+}
